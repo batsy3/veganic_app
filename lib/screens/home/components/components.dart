da8fdc0812256b5724467e1b7146c57d4888f5e5
David utibe-abasi okuku <dvdokuku@gmail.com>
2022-03-28 16:59:19 +0200

    background widget

--- lib/screens/home/components/components.dart
+++ lib/screens/home/components/components.dart
@@ -1,63 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:veganic_app/screens/home/components/background.dart';
 
 class Components extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size; //total height and width of screen
-    return Container(
-      height: size.height,
-      width: double.infinity,
-      child: Stack(
+    return Background(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Positioned(
-              top: 53,
-              left: 27,
-              child: Image.asset(
-                "assets/images/Ellipse_1.png",
-                fit: BoxFit.cover,
-              )),
-          Positioned(
-              top: 105,
-              left: 83,
-              child: Image.asset(
-                "assets/images/Ellipse_2.png",
-                fit: BoxFit.cover,
-              )),
-          Positioned(
-              top: -17,
-              left: 358,
-              child: Image.asset(
-                "assets/images/Ellipse_3.png",
-                fit: BoxFit.cover,
-              )),
-          Positioned(
-              top: 622,
-              left: -22,
-              child: Image.asset(
-                "assets/images/Ellipse_4.png",
-                fit: BoxFit.cover,
-              )),
-          Positioned(
-              top: 561,
-              left: 79,
-              child: Image.asset(
-                "assets/images/Ellipse_5.png",
-                fit: BoxFit.cover,
-              )),
-          Positioned(
-              top: 383,
-              left: 358,
-              child: Image.asset(
-                "assets/images/Ellipse_6.png",
-                fit: BoxFit.cover,
-              )),
-          Positioned(
-              top: 620,
-              left: 391,
-              child: Image.asset(
-                "assets/images/Ellipse_1.png",
-                fit: BoxFit.cover,
-              ))
         ],
       ),
     );
